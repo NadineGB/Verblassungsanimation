158b8aec3689789db5b86d7f2e4452350aa27f40
Nadine Groot-Bernink <nadine.groot-bernink@mediabeam.com>
2020-03-16 13:36:18 +0100

    weiterentwicklungsanfang

--- app/src/main/java/com/example/verblassungsanimation/MainActivity.java
+++ app/src/main/java/com/example/verblassungsanimation/MainActivity.java
@@ -16,11 +16,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void fade(View view) {
+    public void animate(View view) {
 
         Log.i("Info", "ImageView berührt!");
 
         ImageView bearimageView = findViewById(R.id.bear);
+        ImageView catimageView = findViewById(R.id.cat);
+
+        // bearimageView.animate().translationXBy(-500).setDuration(2000); //Image verschiebt sich nach links bei minus (rechts bei plus) Y-Achse verschiebt sich nach oben/unten
+
+        catimageView.animate().rotation(1000).alpha(0).setDuration(2000); //lässt Image rotieren
+
+        bearimageView.animate().scaleX(0.5f).scaleY(0.5f).setDuration(2000); // skaliert das Image kleiner
 
 
     }
