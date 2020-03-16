db6045d5dc52f2e7ce306fd717a8c3e922c6dbdc
Nadine Groot-Bernink <nadine.groot-bernink@mediabeam.com>
2020-03-16 14:22:43 +0100

    animation rotiert rein und rotiert auf berührung raus

--- app/src/main/java/com/example/verblassungsanimation/MainActivity.java
+++ app/src/main/java/com/example/verblassungsanimation/MainActivity.java
@@ -14,13 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView catimageView = findViewById(R.id.cat);
+        catimageView.setX(-1000); // setX -> Position bei Start
+
+        catimageView.animate().translationXBy(1000).rotation(1080).setDuration(2000);
+        // translation -> wohin wird bewegt? / rotation -> rotiert / Duration -> in welcher zeit
     }
 
     public void fade(View view) {
 
         Log.i("Info", "ImageView berührt!");
 
-        ImageView bearimageView = findViewById(R.id.bear);
+        ImageView catimageView = findViewById(R.id.cat);
+
+        catimageView.animate().translationXBy(1000).rotationX(-1000).setDuration(2000);
 
 
     }
